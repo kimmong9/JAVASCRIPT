fa5c2446c1837eb5bd684a160a614c4ec55140d2
kimmong9 <kimay6240@gmail.com>
2022-02-18 10:19:28 +0900

    first commit

---
+++ tutorial01/app.js
@@ -0,0 +1,15 @@
+const price = document.querySelector(".price");
+const count = document.querySelector(".count");
+const sum = document.querySelector(".sum");
+
+const init_Price = 15000;
+const num = 1;
+
+const plus = document.querySelector("price");
+
+price.textContent = init_Price;
+count.textContent = num;
+sum.textContent = init_Price * num;
+
+// plus.addEventListener(이벤트, 해야할 일)
+pl;
